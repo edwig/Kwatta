db10fc039df740e85a5211c34ab12450aa711dbd
Edwig <edwig.huisman@hetnet.nl>
2024-12-07 14:36:00 +0100

    Corrections to clean up all build variants

--- SourceClean.cmd
+++ SourceClean.cmd
@@ -3,19 +3,21 @@ rmdir /q/s bin_release
 rmdir /q/s bin_debugUnicode
 rmdir /q/s bin_releaseUnicode
 rmdir /q/s lib
-
 rmdir /q/s x64
 
-rmdir /q/s Kwatta\x64
-rmdir /q/s Marlin\x64
-rmdir /q/s ResultViewer\x64
-rmdir /q/s StepEditor\x64
-rmdir /q/s StyleFramework\x64
-rmdir /q/s SuiteLibrary\x64
-rmdir /q/s ValidateEditor\x64
-rmdir /q/s TestEditor\x64
-rmdir /q/s TestRunner\x64
-rmdir /q/s Installer\x64
+call :RmSubDirs BaseLibrary
+call :RmSubDirs Installer
+call :RmSubDirs Kwatta
+call :RmSubDirs Marlin
+call :RmSubDirs QLLanguage
+call :RmSubDirs ResultViewer
+call :RmSubDirs SQLComponents
+call :RmSubDirs StepEditor
+call :RmSubDirs StyleFramework
+call :RmSubDirs SuiteLibrary
+call :RmSubDirs TestEditor
+call :RmSubDirs TestRunner
+call :RmSubDirs ValidateEditor
 
 del Installer\Setup_Kwatta*.exe
 
@@ -24,3 +26,17 @@ del /s *.user
 
 attrib -h  .vs
 rmdir /q/s .vs
+
+goto :end
+
+:RmSubDirs
+
+RmDir  /q /s %1\Debug
+RmDir  /q /s %1\DebugUnicode
+RmDir  /q /s %1\Release
+RmDir  /q /s %1\ReleaseUnicode
+RmDir  /q /s %1\x64
+RmDir  /q /s %1\Win32
+goto :end
+
+:end
